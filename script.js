7879d5b0b4264eb683cf94f64ffc6b9f2c7b2b76
Malrett <malte.rettberg@web.de>
2025-09-02 10:49:40 +0200

    small layout modifications for index.html: changed header and footer height, content in main now centered

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ function highlightActiveLink() {
 
 function showSignupScreen() {
   document.getElementById("loginScreen").classList.add("d_none");
-  document.getElementById("indexHeaderRight").classList.add("d_none");
+  document.getElementById("indexHeaderRight").classList.add("hidden");
   document.getElementById("signupScreen").classList.remove("d_none");
 }
 
 function showLoginScreen() {
   document.getElementById("loginScreen").classList.remove("d_none");
-  document.getElementById("indexHeaderRight").classList.remove("d_none");
+  document.getElementById("indexHeaderRight").classList.remove("hidden");
   document.getElementById("signupScreen").classList.add("d_none");
 }
