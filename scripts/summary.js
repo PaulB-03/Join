18220fe31a7131ce1e6e864822a4500ac1d114cd
Arne Klimmt <arne-klimmt@gmx.de>
2025-09-05 14:11:06 +0200

    Current-time now in console summary available

--- scripts/summary.js
+++ scripts/summary.js
@@ -44,6 +44,11 @@ function countForSummary(responseToJson) {
             // changeDateOfUrgentDeadLine(responseToJson.tasks[index].date)
 
         }
+        const now = new Date();
+        const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        console.log(`Today is day ${day} and the time is ${hours}:${minutes}.`);
         changeInnerHtmlOfSummary(progressCount, toDoCount, doneCount, awaitFeedbackCount, urgencyCount)
     }
 }
