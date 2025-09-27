5b4940e2ace8b5fa62e07e19e5b3aa211b544375
Arne Klimmt <arne-klimmt@gmx.de>
2025-09-27 11:15:05 +0200

    header.js logout function  removes showedOnce for summaryGreeting

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,5 @@
 function logout() {
+  localStorage.removeItem("showedOnce");
   localStorage.removeItem("currentUser"); // clear only the login info
   // or localStorage.clear(); if you want to wipe everything
 }
