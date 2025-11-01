c85900ce28b486ff3f8b62cdba4e92e5a5e0e5ab
Arne Klimmt <arne-klimmt@gmx.de>
2025-11-01 12:08:27 +0100

    dopdown category resets and opens correct

--- scripts/dropDown.js
+++ scripts/dropDown.js
@@ -43,18 +43,70 @@ function initDropDownMenue() {
 
   assignedToDropDownContacts.addEventListener("click", openAssignedToContacts)
   assignedToDropdownCategory.addEventListener("click", openCategoryDropDown)
-}
 
+  if (document.getElementById('task-overlay-panel')) {
+    let addTaskOverlayPanel = document.getElementById('task-overlay-panel')
+    addTaskOverlayPanel.addEventListener("click", resetDropDown)
+  } else {
+    let htmlDiv = document.getElementById('htmlDiv')
+    htmlDiv.addEventListener("click", resetDropDown)
+  }
+}
 
 function openAssignedToContacts(event) {
-  let dropDownArrow = event.currentTarget.children[0].children[1]
-  rotateArrowDropDown(dropDownArrow)
+  let assignedToDropDownContacts = document.getElementById('assignedToDropdownContacts')
+  let dropDownArrow = document.getElementById('dropdown-arrow-contacts')
+  event.stopPropagation();
+
+  if (!assignedToDropDownContacts.classList.contains('open')) {
+    resetDropDown("category")
+    assignedToDropDownContacts.classList.add('open')
+    rotateArrowDropDown(dropDownArrow)
+  } else {
+    resetDropDown()
+  }
 }
 
-function openCategoryDropDown() {
-  return console.log('category')
+function openCategoryDropDown(event) {
+  let assignedToDropdownCategory = document.getElementById('assignedToDropdownCategory')
+  let dropDownArrow = document.getElementById('dropdown-arrow-subtasks')
+  event.stopPropagation();
+  // resetDropDown()
+  if (!assignedToDropdownCategory.classList.contains('open')) {
+    resetDropDown("contacts")
+    assignedToDropdownCategory.classList.add('open')
+    rotateArrowDropDown(dropDownArrow)
+  } else {
+    resetDropDown()
+  }
 }
 
 function rotateArrowDropDown(dropDownArrow) {
-  
+  dropDownArrow.style.transform = "rotate(180deg) translateY(50%)"
+}
+
+function resetDropDown() {
+  resetDropDownMenues()
+  resetDropDownArrow()
+}
+
+function resetDropDownMenues(element) {
+  let assignedToDropDownContacts = document.getElementById('assignedToDropdownContacts')
+  let assignedToDropdownCategory = document.getElementById('assignedToDropdownCategory')
+  if (element == "category") {
+    assignedToDropdownCategory.classList.remove('open')
+  } else if (element == "contacts") {
+    assignedToDropDownContacts.classList.remove('open')
+  } else {
+    assignedToDropdownCategory.classList.remove('open')
+    assignedToDropDownContacts.classList.remove('open')
+  }
+
+}
+
+function resetDropDownArrow() {
+  let dropDownArrowContacts = document.getElementById('dropdown-arrow-subtasks')
+  let dropDownArrowAssignedTo = document.getElementById('dropdown-arrow-contacts')
+  dropDownArrowContacts.style = ""
+  dropDownArrowAssignedTo.style = ""
 }
